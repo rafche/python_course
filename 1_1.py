6e6e43cee7628af30cd9b13cd705c55552e5a5fc
Rafche <Rafche@gmail.com>
2017-05-17 20:17:17 +0200

    added files

---
+++ 1_1.py
@@ -0,0 +1,8 @@
+"""
+--> name, <-- greeting in Console
+"""
+
+name = input('please type your name \n')
+
+print('hello {}'.format(name))
+
---
+++ first_lesson.py
@@ -0,0 +1,66 @@
+
+# declaration
+a = 3
+b = 3.14
+c = '3'
+d = 'hello world'
+
+
+
+# print() --> Output Console
+print(a)
+print(b)
+print(c)
+print('------------')
+
+
+# print multiple Items
+print(a, b, c, d)
+print(a, '-', b, '-', c + '-' + d)
+print('------------')
+
+# print string & a number is not working with plus
+# print(a + d)
+# use instead comma
+print(a, b)
+print('------------')
+
+
+# print() --> Output Console
+print('------------')
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print('------------')
+
+# calculation in print statement is possible
+print('------------')
+print(a + b + a * b)
+print(a * a)
+print(a / b)
+print('------------')
+
+#Strings are powerfull
+aString = 'nobody expects the spanish inquisition'
+print(aString)
+print(len(aString))
+
+
+
+
+
+
+#print(b - c) // Type Error
+
+# casting to datatype int, float, string
+print(b - int(c))
+
+
+
+
+
+
+
+
+
