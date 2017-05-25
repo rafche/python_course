04e3df1ddf4ac4a757d0e1ba5034e10858a95b73
FritzPhantom <rafche@gmail.com>
2017-05-25 14:16:55 +0200

    * changes in 1_2_input_name_adv, alternative way for generater class

--- first_lesson/1_2_input_name_adv.py
+++ first_lesson/1_2_input_name_adv.py
@@ -2,8 +2,24 @@
 --> nothing, <-- greeting random name
 User types in Name, getting greet
 """
+# if the module is not known by Pycharm, a red exclamation appears
 import faker
 
-for i in range(0, 100):
+for i in range(0, 10):
     print('hello {}'.format(faker.Factory.create().name()))
     print(i)
+
+
+# it is also possible to other way around
+my_factory = faker.Factory.create()
+
+# my_factory is a Generator object
+# which means it is iteratable from outside the Object / function
+print(type(my_factory))
+
+for i in range(10):
+    print('hello {}'.format(my_factory.name()))
+
+
+
+
