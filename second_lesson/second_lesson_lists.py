538ac16dbbeec43c1c904915f6188052d95eeb4e
Rafche <Rafche@gmail.com>
2017-06-01 21:46:45 +0200

    added some lines in lists

--- second_lesson/second_lesson_lists.py
+++ second_lesson/second_lesson_lists.py
@@ -1,6 +1,43 @@
-# lists are powerfull
+# lists are powerful
+# lists are mutable
+# append and pop
+
+some_list = []
+
+print(some_list)
+
+some_list.append('John')
+
+print(some_list)
+
+some_list.append('Connur')
+
+print(some_list)
+
+some_list.pop(-1)
+
+print(some_list)
+
+some_list.append('Connor')
+
+print(some_list)
 
 
+# iterating over lists
+
+for element in some_list:
+    print(element)
+
+
+# lists can contain everything a tuple can and more
+# for examples tuples
+
+print('\n\n---------------------')
+
+another_list = ['a', 'b', 'c',('inside', 'tuple')]
+
+for element in another_list:
+    print(element)
 
 # let's try some exercises
 # ex_2_1 flat a list of lists to one list object
--- second_lesson/second_lesson_named_tuples.py
+++ second_lesson/second_lesson_named_tuples.py
@@ -1,11 +1,14 @@
 import collections
 
-info_person = collections.namedtuple('person', 'first_name, second_name, age, sex')
+# named tuples are very useful for passing data to functions,....
 
+
+info_person = collections.namedtuple('person', 'first_name, second_name, age, sex')
 
 person_1 = info_person('Sarah','Parker', 40, 'f')
 
 print(person_1)
 
 print('My name is {}'.format(person_1.first_name))
+
 
