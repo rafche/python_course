bc1c3aa6cc3d6640a983ddccd3f8767349c337a7
Rafche <Rafche@gmail.com>
2017-06-07 17:59:41 +0200

    hangman

--- second_lesson/exercise/ex_2_8.py
+++ second_lesson/exercise/ex_2_8.py
@@ -1,21 +1,98 @@
 """
 hangman,
-not finished yet
+nearly finished
 """
+from randomwordgenerator import randomwordgenerator
 
-def check_letter():
-    pass
+def init():
+    '''
+    :return: random word
+    '''
+    return randomwordgenerator.generate_random_words(n=1).lower()
 
-def check_number_of_trys():
-    pass
+
+def masked_word(secret_word: str, letters_guessed: list):
+    mask = ''
+    for letter in secret_word:
+        if letter in letters_guessed:
+            mask += letter
+        else:
+            mask += '-'
+    return mask
+
+
+def available_letters(letters_guessed):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    diff = []
+    diff_formated = ''
+
+    for letter in alphabet:
+        if letter in letters_guessed:
+            diff.append('_')
+        else:
+            diff.append(letter)
+
+    for idx, element in enumerate(diff):
+        if (idx + 1) % 5 == 0:
+            diff_formated += element + ' ' + '\n'
+        else:
+            diff_formated += element + ' '
+
+    return diff_formated
+
+
+def check_word_guessed(secret_word, letters_guessed):
+    '''
+    :param secret_word:
+    :param letters_guessed:
+    :return: true for all letters
+    '''
+    for letter in secret_word:
+        if letter in letters_guessed:
+            pass
+        else:
+            return False
+    return True
+
 
 if __name__ == '__main__':
-    while(1):
+
+    while (1):
+        guesses = 0
+        t_limit = 5
+        known_letters = []
+        word_guessed = False
+
         print('Hi there, do you wanna play hangman?')
-        command =input('[p]lay or [l]eave\n')
+        command = input('[p]lay or [l]eave\n')
 
         if command == 'p':
-            pass
+            secret_word = init()
+            print(secret_word)
+
+            while guesses < t_limit:
+                print(masked_word(secret_word, known_letters))
+                print(' -- available Letters -- ')
+                print(available_letters(known_letters))
+                input_letter = input('guess a letter\n')
+
+                if input_letter in known_letters:
+                    print('letter already guessed')
+                else:
+                    known_letters.append(input_letter)
+
+                if check_word_guessed(secret_word,known_letters):
+                    print('yippie, you won')
+                    break
+
+                if input_letter not in secret_word:
+                    guesses += 1
+            else:
+                print('you lost')
+
         elif command == 'l':
             break
         else:
