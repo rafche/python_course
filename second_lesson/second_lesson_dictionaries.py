19406c3228b014a774f2298d56a46fbe8f7e23ef
FritzPhantom <rafche@gmail.com>
2017-06-10 21:37:49 +0200

    * add dicts

--- second_lesson/second_lesson_dictionaries.py
+++ second_lesson/second_lesson_dictionaries.py
@@ -2,15 +2,21 @@
 # dictionaries eq. associative array
 #
 
-example_dict = {}
+my_dog = {}
 
-print(example_dict)
+print(my_dog)
 
-example_dict['dog'] = 'Beagle'
+my_dog['breed'] = 'Beagle'
+my_dog['age'] = 5
+my_dog['name'] = 'Lynx'
 
-print(example_dict)
+print(my_dog)
 
-example_dict['dog'] = ['Beagle', 'Collie']
+# accessing elements
 
-print(example_dict)
+print('The name of my dog is {}'.format(my_dog['name']))
+print('{} is {} years old'.format(my_dog['name'], my_dog['age']))
 
+# adding Tuples to dict
+
+my_dog['childs']= ['Roofus','Cassiopeia']
