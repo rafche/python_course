7d9e811b5a6efa6b880d07ad8747208347459dae
Rafche <Rafche@gmail.com>
2017-06-15 14:02:56 +0200

    * description added

--- 3_thrid_lesson/exercise/ex_3_1.py
+++ 3_thrid_lesson/exercise/ex_3_1.py
@@ -3,13 +3,15 @@ Enter your Birthday and the return value is your age
 """
 import datetime
 
+
 def get_age(birthday, today):
     return today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+
 
 if __name__ == '__main__':
     birthday = input('please input your birthday DD.MM.YYYY\n')
 
-    if len(birthday.split('.'))>2:
+    if len(birthday.split('.')) > 2:
         birthday_date = datetime.date(year=int(birthday.split('.')[-1]),
                                       month=int(birthday.split('.')[1]),
                                       day=int(birthday.split('.')[0]))
