6c104189685952517d21bf0912e97a114831f85e
Rafche <Rafche@gmail.com>
2017-06-17 12:37:09 +0200

    * adding while loop usage

---
+++ 1_first_lesson/1dex_input_while.py
@@ -0,0 +1,10 @@
+# trapped in while loop till user_input == expected_input:
+
+expected_input = 'x'
+user_input = ''
+
+while user_input != expected_input:
+    user_input = input('press [x]')
+else:
+    print('good bye')
+
