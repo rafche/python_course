bff9ea52df3303e5afbfd958b33998e360794865
Rafche <Rafche@gmail.com>
2017-06-17 13:28:43 +0200

    * adding join in example

--- 1_first_lesson/exercise/ex_1_3.py
+++ 1_first_lesson/exercise/ex_1_3.py
@@ -8,8 +8,11 @@ sentence = str(input('please type in a sentence\n'))
 
 sentence_list = sentence.split(' ')
 
+# using a loop
 for word in reversed(sentence_list):
     reversed_sentence += word + ' '
 
+# using the join method
+print(' '.join(reversed(sentence_list)))
 
 print(reversed_sentence)
