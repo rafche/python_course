6fc842cb176c575b938490fab94263375e684704
Rafche <Rafche@gmail.com>
2017-06-22 18:24:51 +0200

    * new limits

--- 1_first_lesson/exercise/ex_1_7.py
+++ 1_first_lesson/exercise/ex_1_7.py
@@ -7,8 +7,8 @@ import random
 
 
 for i in range(100):
-    y = random.randrange(100)
-    x = random.randrange(100)
+    y = random.randrange(-100, 100)
+    x = random.randrange(-100, 100)
     bin = random.randrange(7)
     result = random.randrange(2)
     print('{:4d}{:4d}{:4d}{:4d}'.format(x, y, bin, result))
