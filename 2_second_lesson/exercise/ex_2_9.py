08bc2a3aceebde688ba70d921ed6d764061e6e74
pris <philipp.reisinger@ams.com>
2017-06-29 13:59:06 +0200

    added duck duck goose

--- 2_second_lesson/exercise/ex_2_9.py
+++ 2_second_lesson/exercise/ex_2_9.py
@@ -1,18 +1,6 @@
-<<<<<<< ccd25684c60b9ebfd5b476fc5178c669c2ae17ec
-
-
-file1 ='awsomefile.xls'
-file2 ='awsomefile.csv'
-file3 ='awsomefile.xlsx'
-
-
-print(file3.split('.')[0])
-
-=======
 def duck_duck_goose(players, goose):
-    return players[goose%len(players)-1]
+    return players[goose % len(players) - 1]
 
 
-players = ['a','b','c','d']
+players = ['a', 'b', 'c', 'd']
 goose = 4
->>>>>>> added duck duck goose
