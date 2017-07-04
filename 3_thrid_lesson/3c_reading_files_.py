8cf83e158afdfef8f69e0205fac23b7f9097e862
Rafche <Rafche@gmail.com>
2017-07-04 21:51:21 +0200

    * parsing hastings

--- 3_thrid_lesson/3c_reading_files_.py
+++ 3_thrid_lesson/3c_reading_files_.py
@@ -1,8 +1,8 @@
-
 def readfile(filename):
     with open(filename, 'r') as f:
         text = f.read()
     return text
+
 
 def wrangling_data(data):
     '''
@@ -11,7 +11,8 @@ def wrangling_data(data):
     '''
     return data.split('\n\n')
 
-def validate_block(block : list):
+
+def validate_block(block: list):
     '''
     :param block: block
     :return: only valid blocks
@@ -22,9 +23,10 @@ def validate_block(block : list):
 
 if __name__ == '__main__':
     data_in_blocks = wrangling_data(readfile('sometext.txt'))
-    valid_blocks = [validate_block(x) for x in data_in_blocks if validate_block(x)is not None]
+    valid_blocks = [validate_block(x) for x in data_in_blocks if validate_block(x) is not None]
 
-
-    for element in valid_blocks:
-        print(element)
-
+    for part in valid_blocks:
+        print('{:~^50}'.format(part.split(':')[0].rstrip().lstrip()))
+        print('{:^50}'.format(part.split(':')[-1].rstrip().lstrip()))
+        print('{:^50}'.format('-'))
+        print('')
