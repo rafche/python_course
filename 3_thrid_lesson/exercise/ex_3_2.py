3c75ca455a6564cca6bd570c4887841b30ab13cd
Rafche <Rafche@gmail.com>
2017-07-05 20:01:13 +0200

    * explained import statement

---
+++ 3_thrid_lesson/exercise/ex_3_2.py
@@ -0,0 +1,11 @@
+# import
+
+import datetime
+
+
+def print_date():
+    print(datetime.date.today())
+
+
+if __name__ == '__main__':
+    print_date()
---
+++ 3_thrid_lesson/exercise/ex_3_3.py
@@ -0,0 +1,11 @@
+# import a special function
+
+from datetime import date
+
+
+def print_date():
+    print(date.today())
+
+
+if __name__ == '__main__':
+    print_date()
