a8165f511bde53d636fd3e7343e5676fdb748d74
Sebastian Rieger <sebastianrieger47@gmail.com>
2018-09-14 14:05:12 +0200

    Add something to the 1a_print_variables

--- 1_first_lesson/1a_print_variables.py
+++ 1_first_lesson/1a_print_variables.py
@@ -13,8 +13,11 @@ pos_float = 3.14159265359
 fake_int = '3'
 string_ = 'hello world'
 neg_float = pos_float * -1
-pos_int += 1
-
+# Mit einer Zahl außerhalb von 1 würde es die Erhöhung besser verdeutlichen.
+# Erklärung int++ funktioniert nicht!
+pos_int += 2
+#Objekt
+#TODO: create an object
 
 
 # print() --> Output Console
@@ -38,7 +41,9 @@ print('------------\n\n')
 # applying a function on a value in print Statement
 # abs() --> print only positive Numbers
 print('------ printing positive Numbers ------')
+print('------ without abs() ------')
 print(neg_float)
+print('------ with abs() ------')
 print(abs(-1*(2*4)))
 print(abs(neg_float))
 print(abs(neg_float + neg_float))
@@ -61,7 +66,7 @@ print('The sum of', pos_int, 'and', neg_int, 'is', pos_int + neg_int)
 print('The sum of {} and {} is {}'.format(pos_int, neg_int, pos_int + neg_int))
 print('The sum of ' + str(pos_int) + ' and ' + str(neg_int) + ' is '+ str(pos_int + neg_int))
 print(f'The sum of {pos_int} and {neg_int} is {pos_int + neg_int}')
-
+print('-----two numbers-------')
 # print string & a number is not working with plus
 # print(a + d)
 # use instead comma
@@ -79,11 +84,13 @@ print('my positive Integer is {}'.format(pos_int))
 print(f'my positive Integer is {pos_int}')
 
 # print() --> Output Console
+#todo: add the object from the top
 print('------ printing the type ------')
 print(type(pos_int))
 print(type(pos_float))
 print(type(fake_int))
 print(type(string_))
+print(type(2))
 print('------------\n\n')
 
 
@@ -104,6 +111,7 @@ print(len(aString))
 
 # Strings can be shorted by stop and start Position
 # where xx: marking the beginning Position and :xx marking the end Position
+print('------ play along with strings ------')
 print(aString[:-11])
 print(aString[:11])
 print(aString[-6:])
@@ -112,7 +120,7 @@ print(aString[6:])
 # sometimes you only need a part of a Sting
 # split() can be the Solution
 # the result of a split is a list
-
+print('------ useing the split function() ------')
 sentence = 'is honduras the same as belize'
 word = sentence.split(' ')
 print(word)
@@ -137,8 +145,9 @@ print('------------\n\n')
 
 
 # formatting strings
-
-# right align, 20 = total width, f = float
+print('------ bring a format to the console ------')
+# ':' is needed
+# right align(no special character needed), 20 = total width, f = float
 print('{:20f}'.format(pos_float))
 
 # center align, 20 = total width, ^ = center, f = float
@@ -164,7 +173,7 @@ print('{:.2%}'.format(pos_float))
 
 print('------------\n\n')
 
-
+print('------ f - string ------')
 # f - strings
 # only in python 3.6
 name = 'Kelly Clarkson'
@@ -176,7 +185,7 @@ print(f'Name of the Song: {title}')
 # remember this string?
 # this String is using the 'older' .format format
 # just hit ALT + Enter and convert to F-String literal
-
+print('------ f - string converting ------')
 print('The sum of {} and {} is {}'.format(pos_int, neg_int, pos_int + neg_int))
 
 # automatically converted to f-sting
@@ -186,16 +195,20 @@ print(f'The sum of {pos_int} and {neg_int} is {pos_int + neg_int}')
 
 
 # swapping variables
-
-b_string = 'Monty'
+print('\n------ f - swapping in one step ------')
+b_string = 'Monthly course'
 c_string = 'Python'
 
-print(b_string, c_string)
+print(f'b_string = "{b_string}",c_string = "{c_string}"')
 
 b_string, c_string = c_string, b_string
 
-print(b_string, c_string)
+print(f'b_string = "{b_string}",c_string = "{c_string}"')
 
+print('\n------ make it pretty ------')
+print('{:<30}'.format(f'b_string = "{b_string}",') + '{:<30}'.format(f'c_string = "{c_string}"'))
+b_string, c_string = c_string, b_string
+print('{:<30}'.format(f'b_string = "{b_string}",') + '{:<30}'.format(f'c_string = "{c_string}"'))
 # let's try some exercises
 # ex_1_1 write an application that adding and printing the length of 2 Strings
 # ex_1_2 write an application that counting the words of an sentence
