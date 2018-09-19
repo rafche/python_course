1ef6d4f624aab34b0dafc41887f40b96a27ab16b
Sebastian Rieger <sebastianrieger47@gmail.com>
2018-09-19 15:50:52 +0200

    add some main classes to structure the code
    Not sure if it's good for a training?!

--- 1_first_lesson/1bex_input_name_adv.py
+++ 1_first_lesson/1bex_input_name_adv.py
@@ -5,16 +5,25 @@ User types in Name, getting greet
 # if the module is not known by Pycharm, a red exclamation appears
 import faker
 
-for i in range(0, 100):
-    print('hello {}'.format(faker.Factory.create().name()))
-    # print(i)
+def onehundredFakeNames():
+    for i in range(0, 100):
+        print('hello {}'.format(faker.Factory.create().name()))
+        # print(i)
 
-# another way
-my_factory = faker.Factory.create()
+def tenFakeNames():
+    # another way
+    my_factory = faker.Factory.create()
 
-for i in range(10):
-    print('hello {}'.format(my_factory.name()))
+    for i in range(10):
+        print('hello {}'.format(my_factory.name()))
 
-print(type(my_factory))
-# my_factory is a Generator object
-# which means it is iterable from outside the Object / function
+def typeOfFaker():
+    my_factory = faker.Factory.create()
+    print(type(my_factory))
+    # my_factory is a Generator object
+    # which means it is iterable from outside the Object / function
+
+if __name__ == "__main__":
+    onehundredFakeNames()
+    tenFakeNames()
+    typeOfFaker()
--- 1_first_lesson/1c_if_else.py
+++ 1_first_lesson/1c_if_else.py
@@ -4,57 +4,80 @@
 pos_int = 16
 limit = 16
 
-
-# if needs always statement which is true or false
-# if else statement don't need the else statement
-if pos_int > limit:
-    print('{} is greater than {}'.format(pos_int, limit))
-
-
-# if else with else statement
-if pos_int < limit:
-    print('{} is less than {}'.format(pos_int, limit))
-else:
-    print('{} is greater than {}'.format(pos_int, limit))
-
-
-# if else elif
-# with the elif statement, more checks are possible
-if pos_int < limit:
-    print('{} is less than {}'.format(pos_int, limit))
-elif pos_int > limit:
-    print('{} is greater than {}'.format(pos_int, limit))
-else:
-    print('{} is equal than {}'.format(pos_int, limit))
-
-
-# also possible, but longer syntax
-if pos_int < limit:
-    print('{} is less than {}'.format(pos_int, limit))
-else:
+def greaterThen():
+    # if needs always statement which is true or false
+    # if else statement don't need the else statement
     if pos_int > limit:
         print('{} is greater than {}'.format(pos_int, limit))
+
+def globalgreaterThenTwoArguments(a,b):
+    # if needs always statement which is true or false
+    # if else statement don't need the else statement
+    if a > b:
+        print('{} is greater than {}'.format(pos_int, limit))
+def localgreaterThenTwoArguments(a,b):
+    # if needs always statement which is true or false
+    # if else statement don't need the else statement
+    if a > b:
+        print('{} is greater than {}'.format(a, b))
+
+def elseStatement():
+    # if else with else statement
+    if pos_int < limit:
+        print('{} is less than {}'.format(pos_int, limit))
     else:
+        print('{} is greater than {}'.format(pos_int, limit))
+
+def elifStatement():
+    # if else elif
+    # with the elif statement, more checks are possible
+    if pos_int < limit:
+        print('{} is less than {}'.format(pos_int, limit))
+    elif pos_int > limit:
+        print('{} is greater than {}'.format(pos_int, limit))
+    else:
+        print('{} is equal than {}'.format(pos_int, limit))
+
+
+def longVersion():
+    # also possible, but longer syntax
+    if pos_int < limit:
+        print('{} is less than {}'.format(pos_int, limit))
+    else:
+        if pos_int > limit:
+            print('{} is greater than {}'.format(pos_int, limit))
+        else:
+            print('{} is equal to {}'.format(pos_int, limit))
+
+
+def andCondition():
+    # combine conditions with 'and' or 'or'
+    # Python doesn't support && or ||
+    if not pos_int < limit and not pos_int > limit:
         print('{} is equal to {}'.format(pos_int, limit))
 
-
-# combine conditions with 'and' or 'or'
-# Python doesn't support && or ||
-
-if not pos_int < limit and not pos_int > limit:
-    print('{} is equal to {}'.format(pos_int, limit))
-
-if pos_int < limit or pos_int > limit:
-    print('{} is NOT equal to {}'.format(pos_int, limit))
+def orCondition():
+    if pos_int < limit or pos_int > limit:
+        print('{} is NOT equal to {}'.format(pos_int, limit))
 
 
-# i use the chance to introduce a new type boolean
-# the Bool can be True or False
+def typeBoolean():
+    # i use the chance to introduce a new type: boolean
+    # the Bool can be True or False
 
-ex_bool = pos_int > limit
-print(ex_bool)
-print(type(ex_bool))
+    ex_bool = pos_int > limit
+    print(ex_bool)
+    print(type(ex_bool))
 
-
+if __name__ == "__main__":
+    greaterThen()
+    elseStatement()
+    elseStatement()
+    longVersion()
+    andCondition()
+    orCondition()
+    typeBoolean()
+    globalgreaterThenTwoArguments(7, 3)
+    localgreaterThenTwoArguments(7, 3)
 # Let's do some exercises
 # ex1_5 type in your full name, an check which name is longer
